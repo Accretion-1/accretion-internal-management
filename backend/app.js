b8fd55183668157bef4da454841729e097de923f
rohan-skyrack <rohan@skyrack.ai>
2026-06-26 15:18:20 +0530

    fix: update static file serving route to serve from root instead of /public

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(
   })
 );
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
 
